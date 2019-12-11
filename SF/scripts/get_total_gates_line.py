02cee97e47f53e7c9396154e92c6b6b0ed362751
devina-singh <devina.singh161@gmail.com>
2019-12-11 07:31:01 -0800

    name Change

--- SF/scripts/get_total_gates_line.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import sys
-
-the_file = open(sys.argv[1],"r")
-for line in the_file.readlines():
-    if line.startswith( 'total_gates' ):
-        print (line)
-the_file.close()
--- SF/test_cases/tests/config/Config_IBMQ_experience.py
+++
@@ -1,2 +0,0 @@
-API_token = '<INSERT_TOKEN_HERE'
-API_URL = 'https://api.quantum-computing.ibm.com/api/Hubs/ibm-q/Groups/open/Projects/main'
